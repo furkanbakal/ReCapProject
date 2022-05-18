ed6a6657cccd3dda47cdb79526bb0461233566b7
f.bakal1997@gmail.com <f.bakal1997@gmail.com>
2022-05-19 00:07:43 +0300

    jwt utilities added

--- Core/Entities/Concrete/OperationClaim.cs
+++ Core/Entities/Concrete/OperationClaim.cs
@@ -1,6 +1,6 @@
 ﻿namespace Core.Entities.Concrete
 {
-    public class OperatinClaim:IEntity
+    public class OperationClaim:IEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
